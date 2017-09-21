05e83c5914be8c42a1fe8d2ecfc57b5e57f6ff82
McAllister <mcal.scroggs@gmail.com>
2017-09-20 20:31:57 -0700

    added a table section. Also added scrollbars that are hidden when not needed. Made table resizable.

--- TruthTableGenerator.py
+++ TruthTableGenerator.py
@@ -1,8 +1,9 @@
 import sys
 import os
-from tkinter import Tk, Button, Entry, E, W, N, S, INSERT, PhotoImage, Menu
-from tkinter import StringVar
-
+from tkinter import Tk, Button, Entry, E, W, N, S, INSERT, PhotoImage, Menu, \
+    StringVar, Label, Frame, Canvas, GROOVE
+from AutoScrollBar import AutoScrollbar
+program_directory = sys.path[0]
 
 
 def insert_text(field, text):
@@ -10,20 +11,9 @@ def insert_text(field, text):
     return
 
 
+    
 def validate(text):
     raise Exception("Input recieved: {}".format(text))
-
-
-def generate(textfield):
-    try:
-        validate(textfield.get())
-        print("VALID")
-    except Exception as ex:
-        print(str(ex))
-
-
-def callback(sv):
-    print(sv.get())
 
 
 def test():
@@ -31,12 +21,14 @@ def test():
 
 
 class TruthTableGeneratorGUI:
-    def __init__(self, master):
-        self.master = master
-        master.title("Truth Table Generator")
+    def __init__(self, root):
+        self.root = root
+        root.title("Truth Table Generator")
+        root.iconphoto(True, PhotoImage(file=os.path.join(program_directory,
+                                                          "icon.png")))
 
         # create a menu bar
-        menubar = Menu(master)
+        menubar = Menu(root)
 
         # create the file menu
         filemenu = Menu(menubar, tearoff=0)
@@ -52,49 +44,73 @@ class TruthTableGeneratorGUI:
         menubar.add_cascade(label="Help", menu=helpmenu)
         menubar.add_command(label="test", command=test)
 
-        master.config(menu=menubar)
+        root.config(menu=menubar)
+
+        # create the table area and scrollbars
+        tableframe = Frame(root, relief=GROOVE, padx=5, pady=5, bd=2)
+        tableframe.grid(row=3, column=0, columnspan=5, sticky=E+W+N+S)
+
+        root.grid_rowconfigure(3, weight=1)
+        tableframe.grid_rowconfigure(0, weight=1)
+        tableframe.grid_columnconfigure(0, weight=1)
+
+        # make the scroll bars
+        tablevsb = AutoScrollbar(tableframe, orient="vertical")
+        tablehsb = AutoScrollbar(tableframe, orient="horizontal")
+
+        tablevsb.grid(row=0, column=1, sticky=N+S)
+        tablehsb.grid(row=1, column=0, sticky=E+W)
+
+        # make the canvas that will hold the window with the table in it
+        self.tablecanvas = Canvas(tableframe,
+                                  yscrollcommand=tablevsb.set,
+                                  xscrollcommand=tablehsb.set)
+        self.tablecanvas.grid(row=0, column=0, sticky=N+S+E+W)
+
+        tablevsb.config(command=self.tablecanvas.yview)
+        tablehsb.config(command=self.tablecanvas.xview)
 
         # declaring and defining widgets
         sv = StringVar()
         sv.trace("w", lambda name, index, mode, sv=sv: generate(sv))
-        self.entry = Entry(master, textvariable=sv)
+        self.entry = Entry(root, textvariable=sv)
 
-        self.p_button = Button(master, text="p", width=1,
+        self.p_button = Button(root, text="p", width=1,
                                command=lambda: insert_text(self.entry, 'p '))
 
-        self.q_button = Button(master, text="q", width=1,
+        self.q_button = Button(root, text="q", width=1,
                                command=lambda: insert_text(self.entry, 'q '))
 
-        self.and_button = Button(master, text="AND", width=1,
+        self.and_button = Button(root, text="AND", width=1,
                                  command=lambda: insert_text(self.entry,
                                                              ' /\ '))
 
-        self.or_button = Button(master, text="OR", width=1,
+        self.or_button = Button(root, text="OR", width=1,
                                 command=lambda: insert_text(self.entry,
                                                             ' \/ '))
 
-        self.not_button = Button(master, text="NOT", width=1,
+        self.not_button = Button(root, text="NOT", width=1,
                                  command=lambda: insert_text(self.entry,
                                                              ' ~ '))
 
-        self.left_paren_button = Button(master, text="(", width=1,
+        self.left_paren_button = Button(root, text="(", width=1,
                                         command=lambda: insert_text(self.entry,
                                                                     '('))
 
-        self.right_paren_button = Button(master, text=")", width=1,
+        self.right_paren_button = Button(root, text=")", width=1,
                                          command=lambda:
                                          insert_text(self.entry, ')'))
 
-        self.if_button = Button(master, text="IF", width=1,
+        self.if_button = Button(root, text="IF", width=1,
                                 command=lambda: insert_text(self.entry,
                                                             ' -> '))
 
-        self.iff_button = Button(master, text="IFF", width=1,
+        self.iff_button = Button(root, text="IFF", width=1,
                                  command=lambda: insert_text(self.entry,
                                                              ' <-> '))
 
-        self.generate_button = Button(master, text="GO!",
-                                      command=lambda: generate(sv))
+        self.generate_button = Button(root, text="GO!",
+                                      command=self.generate_table)
 
         # adding widgets to the grid
         self.p_button.grid(row=1, column=0, sticky=E+W+N+S)
@@ -109,11 +125,31 @@ class TruthTableGeneratorGUI:
         self.generate_button.grid(row=0, column=4, sticky=E+W)
         self.entry.grid(row=0, column=0, columnspan=4, sticky=E+W+N+S)
 
-        master.columnconfigure(0, weight=1)
-        master.columnconfigure(1, weight=1)
-        master.columnconfigure(2, weight=1)
-        master.columnconfigure(3, weight=1)
-        master.columnconfigure(4, weight=1)
+        root.grid_columnconfigure(0, weight=1)
+        root.grid_columnconfigure(1, weight=1)
+        root.grid_columnconfigure(2, weight=1)
+        root.grid_columnconfigure(3, weight=1)
+        root.grid_columnconfigure(4, weight=1)
+
+    def generate_table(self):
+        # clear the canvas of past tables
+        self.tablecanvas.delete("all")
+
+        # create the frame to hold the table
+        frame = Frame(self.tablecanvas)
+        frame.rowconfigure(0, weight=1)
+        frame.columnconfigure(0, weight=1)
+
+        # PLACE HOLDER FOR REAL TABLE GENERATION
+        for i in range(25):
+            Button(frame, text=i).grid(row=i, column=i)
+
+        # make the window that holds the frame
+        self.tablecanvas.create_window(0, 0, window=frame, anchor=N+W)
+        frame.update_idletasks()
+
+        # update the scrollable area to the new width
+        self.tablecanvas.config(scrollregion=self.tablecanvas.bbox("all"))
 
 
 if __name__ == "__main__":
@@ -121,9 +157,7 @@ if __name__ == "__main__":
     # root.resizable(width=False, height=False)
     # root.geometry('{}x{}'.format(400, 500))
 
-    program_directory = sys.path[0]
-    root.iconphoto(True, PhotoImage(file=os.path.join(program_directory,
-                                                      "icon.png")))
+    # create the GUI
     gui = TruthTableGeneratorGUI(root)
 
     # start the main loop
